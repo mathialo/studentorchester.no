9f0d59c17d08af1155d38b22ecba015cd8e7b34a
Mathias Lohne <mathialo@student.matnat.uio.no>
2017-05-30 00:28:50 +0200

    fjernet noen av de mindre morsomme spydighetene

--- js/spydig.js
+++ js/spydig.js
@@ -3,9 +3,7 @@ function addSpydigKommentar() {
 		". Ædda bædda, vi kom først &nbsp;<i>#domeneparkering</i>",
 		", som åpenbart er det beste orchesteret i landet",
 		". Hurra, bravo, vi klarte det!",
-		". Mere, mere, mere!",
-		" - norgesmestere i Holmenkollmarsj (44 sek).",
-		". Begynn i Biørneblæs."
+		" - norgesmestere i Holmenkollmarsj (44 sek)."
 	];
 
 	document.getElementById("spydigKommentar").innerHTML = comments[Math.floor(Math.random()*comments.length)];
